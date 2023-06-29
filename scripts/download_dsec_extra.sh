c2575e6a958e329ccde2255bedd6f4fd7051f82b
Mathias Gehrig <mathias.gehrig@gmail.com>
2023-06-29 14:08:24 +0200

    Update download_dsec_extra.sh
    rm disparity download from dsec extra

--- scripts/download_dsec_extra.sh
+++ scripts/download_dsec_extra.sh
@@ -3,7 +3,7 @@ DSEC_ROOT=$1
 for split in test train; do
     mkdir -p $DSEC_ROOT/$split
     cd $DSEC_ROOT/$split || exit
-    for mod in events images disparity calibration object_detections; do
+    for mod in events images calibration object_detections; do
         filename="$split"_"$mod".zip
         wget https://download.ifi.uzh.ch/rpg/DSEC/"$split"_object_detection_coarse/$filename
         unzip $filename
